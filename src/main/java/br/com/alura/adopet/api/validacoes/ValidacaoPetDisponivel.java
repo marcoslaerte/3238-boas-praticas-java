de893baf947510f3c8ba2e466dadfdbc0236510b
Marcos Laerte Gomes de Lima <marcos.laerte.lima@gmail.com>
2026-05-12 16:25:39 -0300

    Aplicando o padrão Strategy: separando cada validador em classes diferentes, cada uma com responsabilidade única

---
+++ src/main/java/br/com/alura/adopet/api/validacoes/ValidacaoPetDisponivel.java
@@ -0,0 +1,22 @@
+package br.com.alura.adopet.api.validacoes;
+
+import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
+import br.com.alura.adopet.api.exception.ValidacaoException;
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.repository.PetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component //Para o Spring poder utilizar uma classe de validação de dados, pode-se usar a anotação @Component (poderia ser @Service também, já que são parecidos)
+public class ValidacaoPetDisponivel {
+
+    @Autowired
+    private PetRepository petRepository;
+
+    public void validar(SolicitacaoAdocaoDto dto) {
+        Pet pet = petRepository.getReferenceById(dto.idPet());
+        if (pet.getAdotado()) {
+            throw new ValidacaoException("Pet já foi adotado!");
+        }
+    }
+}
